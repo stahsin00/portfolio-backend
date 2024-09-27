ca18244a6915dcf42531f44fb4761b710aaf8349
BuildTools <lamisa1998@hotmail.com>
2024-09-27 16:19:34 -0700

    start on chat

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/contact', require('./middleware/routes/api/contact'));
+app.use('/api/chat', require('./middleware/routes/api/chat'));
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!!!`));
